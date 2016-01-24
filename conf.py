c5ec046cd2d93763575c3ba46172ae5fcb6cf40d
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-01-24 18:48:06 -0400

    Attempting to make references anonymous on ht.conv_jacket #5

--- conf.py
+++ conf.py
@@ -50,8 +50,11 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'numpydoc'
 ]
+
+#     'sphinx.ext.napoleon'
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
