25691aae5437d8dc00039bafa25448625843aab6
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2018-07-19 21:27:04 -0600

    Add another function for air cooler noise

--- tests/test_air_cooler.py
+++ tests/test_air_cooler.py
@@ -35,6 +35,22 @@ def test_air_cooler_Ft():
     Ft_values = [[0.6349871996666123, 0.9392743008890244, 0.9392743008890244, 0.9392743008890244, 0.9392743008890244], [0.7993839562360742, 0.9184594715750571, 0.9392743008890244, 0.9392743008890244, 0.9392743008890244], [0.8201055328279105, 0.9392743008890244, 0.9784008071402877, 0.9392743008890244, 0.9392743008890244], [0.8276966706732202, 0.9392743008890244, 0.9392743008890244, 0.9828365967034366, 0.9392743008890244], [0.8276966706732202, 0.9392743008890244, 0.9392743008890244, 0.9392743008890244, 0.9828365967034366]]
     assert_allclose(Ft_many, Ft_values)
     
+    
 def test_air_cooler_noise_GPSA():
     noise = air_cooler_noise_GPSA(tip_speed=3177/minute, power=25.1*hp)
     assert_allclose(noise, 100.53680477959792)
+    
+    
+def test_air_cooler_noise_Mukherjee():
+    '''    # Confirmed to be log10's because of example tip speed reduction
+    # of 60 m/s to 40 m/s saves 5.3 dB.
+    # hp in shaft horse power
+    # d in meters
+    # sound pressure level, ref level 2E-5 pa
+
+    '''
+    noise = air_cooler_noise_Mukherjee(tip_speed=3177/minute, power=25.1*hp, fan_diameter=4.267)
+    assert_allclose(noise, 99.11026329092925)
+    
+    noise = air_cooler_noise_Mukherjee(tip_speed=3177/minute, power=25.1*hp, fan_diameter=4.267, induced=True)
+    assert_allclose(noise, 96.11026329092925)
