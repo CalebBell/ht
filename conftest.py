d64717a157949827aa60c7b086564844ae075795
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-04-22 20:20:17 -0600

    Attempt to re-enable numba CI

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def pytest_ignore_collect(path):
         return True
     ver_tup = platform.python_version_tuple()[0:2]
     ver_tup = tuple(int(i) for i in ver_tup)
-    if ver_tup < (3, 6) or ver_tup >= (3, 10) or is_pypy:
+    if ver_tup < (3, 7) or ver_tup >= (3, 10) or is_pypy:
         # numba does not yet run under pypy
         if 'numba' in path:
             return True
