baad38000390005aceb25379fde756a79773f0b8
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2025-10-14 22:26:16 -0600

    cleanup

---
+++ dev/basic_standalone_ht_check.py
@@ -0,0 +1,23 @@
+import ht
+from ht import *
+import numpy as np
+import scipy.integrate
+import scipy.interpolate
+import scipy.spatial
+import scipy.special
+import scipy.optimize
+
+def check_close(a, b, rtol=1e-7, atol=0):
+    np.all(np.abs(a - b) <= (atol + rtol * np.abs(b)))
+    return True
+
+def run_checks():
+    checks = []
+
+    return all(checks)
+
+if run_checks():
+    print("ht basic checks passed - NumPy and SciPy used successfully")
+else:
+    print('Library not OK')
+    exit(1)
